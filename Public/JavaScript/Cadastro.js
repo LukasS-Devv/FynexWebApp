781f2eb4d4c4770f0c8500041806e0d00c1f419a
Lucas Silva <lucas.silva.maia.08@gmail.com>
2026-02-08 16:22:51 +0000

    Update de paginas

--- Public/JavaScript/Cadastro.js
+++ Public/JavaScript/Cadastro.js
@@ -1,4 +1,17 @@
 function Entrar ()
 {
-    window.open("../Html/BemVindo.html","_self")
+    const nome = document.getElementById("nome");
+    const sobrenome = document.getElementById("sobrenome");
+    const montante = document.getElementById("montante");
+    const moeda = document.getElementById("moeda");
+
+    if (nome.value.trim() === "" || sobrenome.value.trim() === "" || montante.value.trim() === "" || moeda.value === "N")
+    {
+        alert("Preencha todos os campos");
+        return;
+    }
+    
+    window.open("../Html/BemVindo.html","_self");
 };
+
+
